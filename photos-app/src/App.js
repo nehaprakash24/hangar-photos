804a6776f003611a7b96b716af088856e5d6bd3d
Neha Prakash <neha.prakash266@gmail.com>
2020-07-23 01:54:47 +0530

    Fixed router div height

--- photos-app/src/App.js
+++ photos-app/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import { Router, Link } from "@reach/router";
+import { Router } from "@reach/router";
 import Home from './Components/Home';
 import Search from './Components/Search';
 
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router className="router-height">
         <Home path="/" />
         <Search path="/search" />
       </Router>
