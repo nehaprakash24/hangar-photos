f6ab2f0e07a371f28338be14fb0fe7473cad9c3d
Neha Prakash <neha.prakash266@gmail.com>
2020-07-28 02:51:28 +0530

    Updates

---
+++ precache-manifest.c5cecac231e8577b5769f3614ac3b68a.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ce391a75f980c3ee5682b8f6fde09f6",
+    "url": "/hangar-photos/photos-app/index.html"
+  },
+  {
+    "revision": "86ee710b8e0d653cf94b",
+    "url": "/hangar-photos/photos-app/static/css/2.f87e9f44.chunk.css"
+  },
+  {
+    "revision": "b1e537db634ec6d2a1a0",
+    "url": "/hangar-photos/photos-app/static/css/main.d5c0f637.chunk.css"
+  },
+  {
+    "revision": "86ee710b8e0d653cf94b",
+    "url": "/hangar-photos/photos-app/static/js/2.ed1ab23c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangar-photos/photos-app/static/js/2.ed1ab23c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e537db634ec6d2a1a0",
+    "url": "/hangar-photos/photos-app/static/js/main.76b5593c.chunk.js"
+  },
+  {
+    "revision": "6b2deafc90a36d5a4664",
+    "url": "/hangar-photos/photos-app/static/js/runtime-main.f0c50d35.js"
+  },
+  {
+    "revision": "1cc987cbe1dd320885df26c6a8632f43",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Bold-webfont.1cc987cb.ttf"
+  },
+  {
+    "revision": "317ed94a878c8d8ea413f51e575513f4",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Bold-webfont.317ed94a.woff"
+  },
+  {
+    "revision": "3cdf602b3281ee10ef3c7260cafd634f",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Bold-webfont.3cdf602b.otf"
+  },
+  {
+    "revision": "9df23ffb1fc0a629005df32644a24b70",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Bold-webfont.9df23ffb.woff2"
+  },
+  {
+    "revision": "bccd23a84e30eb286d748dc86b09c49d",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Bold-webfont.bccd23a8.eot"
+  },
+  {
+    "revision": "deb29bc75156be488415ac59f333d71c",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Bold-webfont.deb29bc7.svg"
+  },
+  {
+    "revision": "621ba5aaf66e52f3def0cf69807104b9",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Heavy-webfont.621ba5aa.otf"
+  },
+  {
+    "revision": "6dab36a9692b36d02984ee95716b7ef7",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Heavy-webfont.6dab36a9.woff2"
+  },
+  {
+    "revision": "977a8badf138ba0183b618103fbea86a",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Heavy-webfont.977a8bad.woff"
+  },
+  {
+    "revision": "a765488db4d152dad4968f4faf02c126",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Heavy-webfont.a765488d.ttf"
+  },
+  {
+    "revision": "cbca114bace1dcafcba0ccf9b91c56f4",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Heavy-webfont.cbca114b.eot"
+  },
+  {
+    "revision": "fa87079ec53550641957a5b3725802f9",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Heavy-webfont.fa87079e.svg"
+  },
+  {
+    "revision": "20b648f197c843b978ab18535d7b2b4b",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Light-webfont.20b648f1.woff2"
+  },
+  {
+    "revision": "35ede60c5aa47c2262853af06ed0c28c",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Light-webfont.35ede60c.eot"
+  },
+  {
+    "revision": "7120f405f94a79633cb8671daf076264",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Light-webfont.7120f405.otf"
+  },
+  {
+    "revision": "c8c8da05d4a5bea206f0a9da69f2963a",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Light-webfont.c8c8da05.ttf"
+  },
+  {
+    "revision": "da75003ce72fcafb82668aa8d9eb1bba",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Light-webfont.da75003c.woff"
+  },
+  {
+    "revision": "f095fbfe098e724b2964b8a31587f45a",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Light-webfont.f095fbfe.svg"
+  },
+  {
+    "revision": "4309f5e6504ab4404a1c909a5ef8457f",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Regular-webfont.4309f5e6.woff"
+  },
+  {
+    "revision": "74bbbab2c7f147a278ebc9866b5394de",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Regular-webfont.74bbbab2.svg"
+  },
+  {
+    "revision": "9f1d7341a18b9de6061ecdf4a9adcc72",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Regular-webfont.9f1d7341.eot"
+  },
+  {
+    "revision": "c9b9f1d8f6826c2683987a76e73adea6",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Regular-webfont.c9b9f1d8.woff2"
+  },
+  {
+    "revision": "d321fa78bb7190a8ca7e14213ef63203",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Regular-webfont.d321fa78.otf"
+  },
+  {
+    "revision": "e0fde64e04cb74353bdd55f6af7bd6f7",
+    "url": "/hangar-photos/photos-app/static/media/Aileron-Regular-webfont.e0fde64e.ttf"
+  },
+  {
+    "revision": "778883f2dfc84bc7b789d1bc557bf7e7",
+    "url": "/hangar-photos/photos-app/static/media/arrow.778883f2.svg"
+  },
+  {
+    "revision": "c6d08ceb6819d2df8cb4e3be84cc372b",
+    "url": "/hangar-photos/photos-app/static/media/facebook.c6d08ceb.svg"
+  },
+  {
+    "revision": "5f1cfe60abca790df8adffbca27fb9aa",
+    "url": "/hangar-photos/photos-app/static/media/hangar_logo_large.5f1cfe60.svg"
+  },
+  {
+    "revision": "95143775b529d8c842032beaa0dd29d7",
+    "url": "/hangar-photos/photos-app/static/media/hangar_logo_small.95143775.svg"
+  },
+  {
+    "revision": "baab862890c1a6cc926ddb8292e8f998",
+    "url": "/hangar-photos/photos-app/static/media/linkedin.baab8628.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hangar-photos/precache-manifest.135f1c60695cd5e5fe38ee67c2da8a6a.js"
+  "/hangar-photos/photos-app/precache-manifest.c5cecac231e8577b5769f3614ac3b68a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hangar-photos/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hangar-photos/photos-app/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
